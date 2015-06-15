0aa4888b7415f3acb53717057b1af7ec0ca8ea1c
André Luiz de Amorim <streetomon@gmail.com>
2015-06-15 15:05:45 -0300

    Fixed bug in last column of master list reader.

--- diving3d/masterlist.py
+++ diving3d/masterlist.py
@@ -8,7 +8,18 @@ Created on 15/06/2015
 __all__ = ['read_masterlist']
 
 
-dtype=[('id', '|S05'), ('name', '|S12'), ('V_hel', 'float64'), ('morph', '|S05'), ('T', 'float64'), ('R_e', 'float64'), ('M_K', 'float64'), ('n_s', 'float64'), ('epsilon', 'float64'), ('DL', 'float64'), ('eDL', 'float64'), ('EL', 'float64')]
+dtype=[('id', '|S05'),
+       ('name', '|S12'),
+       ('V_hel', 'float64'),
+       ('morph', '|S05'),
+       ('T', 'float64'),
+       ('R_e', 'float64'),
+       ('M_K', 'float64'),
+       ('n_s', 'float64'),
+       ('epsilon', 'float64'),
+       ('DL', 'float64'),
+       ('eDL', 'float64'),
+       ('EL', '|S05')]
 
 
 def read_masterlist(filename):
