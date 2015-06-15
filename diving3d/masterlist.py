9945ac70af159dbaf1d23bb4561bc3b464460ed3
André Luiz de Amorim <streetomon@gmail.com>
2015-06-15 14:07:53 -0300

    Added master list table and reader.

---
+++ diving3d/masterlist.py
@@ -0,0 +1,16 @@
+'''
+Created on 15/06/2015
+
+@author: andre
+'''
+
+
+__all__ = ['read_masterlist']
+
+
+dtype=[('id', '|S05'), ('name', '|S12'), ('V_hel', 'float64'), ('morph', '|S05'), ('T', 'float64'), ('R_e', 'float64'), ('M_K', 'float64'), ('n_s', 'float64'), ('epsilon', 'float64'), ('DL', 'float64'), ('eDL', 'float64'), ('EL', 'float64')]
+
+
+def read_masterlist(filename):
+    import numpy as np
+    return np.genfromtxt(filename, dtype, skip_header=2)
