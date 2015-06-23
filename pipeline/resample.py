e405db8d1d07a40986b4aed89481b7c50589eb1d
André Luiz de Amorim <streetomon@gmail.com>
2015-06-23 16:32:09 -0300

    Added a sample script to test homogenized (please find a better name) cube.

--- pipeline/resample.py
+++ pipeline/resample.py
@@ -15,23 +15,8 @@ def velocity_to_redshift(v):
     c = 299792458.0 # km/s
     return v / c
 
-def plot_example(d3d, galaxyId, y_slice):
-    import matplotlib.pyplot as plt
-    plt.ioff()
-    xx = d3d.x_coords
-    yy = d3d.y_coords
-    ll = d3d.l_obs
-    plt.pcolormesh(xx, ll, d3d.f_flag[:, y_slice, :])
-    plt.xlabel(r'R.A. [arcsec]')
-    plt.ylabel(r'Wavelength [$\AA$]')
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(ll.min(), ll.max())
-    plt.title('%s - flags example @ dec = %.02f "' % (galaxyId, yy[y_slice]))
-    plt.colorbar()
-    plt.show()
-
 # TODO: read resampling setup from a config file
-debug = False
+overwrite = False
 redcube = 'data/cubes/T001_AV_r_d_NIT_6_fft_x_0pt15_y_0pt15_n_eq_6_bg_rec_wav_rec_pca_dop_hel.fits'
 resamcube = 'data/cubes/T001_resampled.fits'
 masterlist = 'data/masterlist_sampleT.txt'
@@ -54,7 +39,4 @@ maskfile = mask_template % ml['grating']
 gap_mask = get_wavelength_mask(maskfile, d3d.l_obs, z, dest='rest')
 d3d.f_flag[gap_mask] |= flags.ccd_gap
 
-if debug:
-    plot_example(d3d, galaxyId, y_slice=10)
-
-d3d.write(resamcube)
+d3d.write(resamcube, overwrite=overwrite)
---
+++ scratch/test_cube.py
@@ -0,0 +1,30 @@
+'''
+Created on 23/06/2015
+
+@author: andre
+'''
+
+from diving3d.cube import D3DFitsCube
+from diving3d.tables import get_galaxy_id
+
+def plot_example(d3d, galaxyId, y_slice):
+    import matplotlib.pyplot as plt
+    plt.ioff()
+    xx = d3d.x_coords
+    yy = d3d.y_coords
+    ll = d3d.l_obs
+    plt.pcolormesh(xx, ll, d3d.f_flag[:, y_slice, :])
+    plt.xlabel(r'R.A. [arcsec]')
+    plt.ylabel(r'Wavelength [$\AA$]')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(ll.min(), ll.max())
+    plt.title('%s - flags example @ dec = %.02f "' % (galaxyId, yy[y_slice]))
+    plt.colorbar()
+    plt.show()
+
+
+cube = 'data/cubes/T001_resampled.fits'
+galaxy_id = get_galaxy_id(cube)
+d3d = D3DFitsCube(cube)
+
+plot_example(d3d, galaxy_id, y_slice=10)
