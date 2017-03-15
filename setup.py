ad326efb12f43d5dc534e7b4cb6572251e211b6a
Daniel Ruschel Dutra <druscheld@gmail.com>
2017-03-15 16:14:39 -0300

    Added setup.py file to allow installation via pip.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from numpy.distutils.core import setup
+
+packdata = {}
+packdata['pycasso2'] = ['examples/*']
+
+setup(
+    name='pycasso2',
+    version=0.1,
+    packages=['pycasso2'],
+    scripts=[
+        'scripts/pycasso_explorer.py',
+        'scripts/pycasso_import.py',
+        'scripts/pycasso_starlight.py',
+        ],
+    description="Pycasso2",
+    author="André Amorin",
+    author_email="streetomon@gmail.com",
+    url='https://bitbucket.org/streeto/pycasso2',
+    platform='Linux',
+    license='GPLv3',
+)
