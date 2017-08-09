aca757be4eb3c7199e57a26269b2626e11a3e054
André Luiz de Amorim <streetomon@gmail.com>
2017-08-09 11:05:36 -0300

    Fixed CALIFA masterlist reader.

--- pycasso2/importer/califa.py
+++ pycasso2/importer/califa.py
@@ -9,7 +9,7 @@ from .core import import_spectra, safe_getheader, fill_cube
 
 from astropy import log, wcs
 from astropy.io import fits
-from astropy.table import Table
+from astropy.table import Table, Column
 import numpy as np
 
 __all__ = ['read_califa', 'califa_read_masterlist']
@@ -74,12 +74,144 @@ def califa_read_masterlist(filename, galaxy_id=None):
         A numpy record array containing either the whole masterlist
         or the entry pointed by ``galaxy_id``.
     '''
-    ml = Table.read(filename, format='csv')
+    ml = Table()
+    rec = np.genfromtxt(filename, dtype=_dtype, skip_header=1, delimiter=',')
+    for name in rec.dtype.names:
+        c = Column(rec[name], name=name)
+        ml.add_column(c)
+
     if galaxy_id is not None:
-        index = np.where(ml['#CALIFA_ID'] == galaxy_id)[0]
+        index = np.where(ml['CALIFA_ID'] == galaxy_id)[0]
         if len(index) == 0:
-            raise Exception(
-                'Entry %s not found in masterlist %s.' % (galaxy_id, filename))
+            raise Exception('Entry %s not found in masterlist %s.' % (galaxy_id, filename))
         return ml[index][0]
     else:
         return ml
+
+
+_dtype = np.dtype([
+         ('CALIFA_ID', 'S20'),
+         ('ned_name', 'S20'),
+         ('ppak_file', 'S40'),
+         ('ra', np.float64),
+         ('dec', np.float64),
+         ('b', np.float64),
+         ('l', np.float64),
+         ('d_Mpc', np.float64),
+         ('modz', np.float64),
+         ('INFOmodz', 'S1'),
+         ('IRSA_E(B-V)', np.float64),
+         ('e_IRSA_E(B-V)', np.float64),
+         ('Mr', np.float64),
+         ('u-r', np.float64),
+         ('objID', np.int64),
+         ('run', np.int),
+         ('rerun', np.int),
+         ('camcol', np.int),
+         ('field', np.int),
+         ('obj', np.int),
+         ('type', np.int),
+         ('flags', np.int64),
+         ('fiberMag_r', np.float64),
+         ('petroMag_u', np.float64),
+         ('petroMag_g', np.float64),
+         ('petroMag_r', np.float64),
+         ('petroMag_i', np.float64),
+         ('petroMag_z', np.float64),
+         ('extinction_u', np.float64),
+         ('extinction_g', np.float64),
+         ('extinction_r', np.float64),
+         ('extinction_i', np.float64),
+         ('extinction_z', np.float64),
+         ('petroRad_r', np.float64),
+         ('petroR50_r', np.float64),
+         ('petroR90_r', np.float64),
+         ('isoA_g', np.float64),
+         ('isoB_g', np.float64),
+         ('isoA_r', np.float64),
+         ('isoB_r', np.float64),
+         ('isoAgrad_r', np.float64),
+         ('isoBgrad_r', np.float64),
+         ('specObjID', np.int64),
+         ('z', np.float64),
+         ('zErr', np.float64),
+         ('zConf', np.float64),
+         ('zStatus', np.int),
+         ('specClass', np.int),
+         ('velDisp', np.float64),
+         ('velDispErr', np.float64),
+         ('eClass', np.float64),
+         ('J', np.float64),
+         ('eJ', np.float64),
+         ('H', np.float64),
+         ('eH', np.float64),
+         ('K', np.float64),
+         ('eK', np.float64),
+         ('FUV', np.float64),
+         ('eFUV', np.float64),
+         ('NUV', np.float64),
+         ('eNUV', np.float64),
+         ('Fnu_12', np.float64),
+         ('Fnu_25', np.float64),
+         ('Fnu_60', np.float64),
+         ('Fnu_100', np.float64),
+         ('NEDNAME', 'S20'),
+         ('CHANDRA_flux_aper_b', np.float64),
+         ('CHANDRA_hard_hs', np.float64),
+         ('mjd', np.int),
+         ('plate', np.int),
+         ('fiberid', np.int),
+         ('FIRST_flux_int', np.float64),
+         ('FIRST_e_flux_int', np.float64),
+         ('FIRST_MajAx', np.float64),
+         ('FIRST_MinAx', np.float64),
+         ('FIRST_PosAng', np.float64),
+         ('abs_U', np.float64),
+         ('abs_G', np.float64),
+         ('abs_R', np.float64),
+         ('abs_I', np.float64),
+         ('abs_Z', np.float64),
+         ('abs_J', np.float64),
+         ('abs_H', np.float64),
+         ('abs_K', np.float64),
+         ('eabs_U', np.float64),
+         ('eabs_G', np.float64),
+         ('eabs_R', np.float64),
+         ('eabs_I', np.float64),
+         ('eabs_Z', np.float64),
+         ('eabs_J', np.float64),
+         ('eabs_H', np.float64),
+         ('eabs_K', np.float64),
+         ('MU50R', np.float64),
+         ('PETROTHETA', np.float64),
+         ('PETROTH50', np.float64),
+         ('PETROTH90', np.float64),
+         ('SERSIC_N', np.float64),
+         ('SERSIC_TH50', np.float64),
+         ('SERSIC_FLUX', np.float64),
+         ('CLASS', 'S20'),
+         ('SUBCLASS', 'S20'),
+         ('int_r_mag', np.float64),
+         ('_PA', np.float64),       # Original name: 'PA'
+         ('_PA_align', np.float64), # Original name: 'PA_align'
+         ('_PA_err', np.float64),   # Original name: 'PA_err'
+         ('_BA', np.float64),       # Original name: 'BA'
+         ('_BA_err', np.float64),   # Original name: 'BA_err'
+         ('re', np.float64),
+         ('r90', np.float64),
+         ('r20', np.float64),
+         ('u_mag', np.float64),
+         ('g_mag', np.float64),
+         ('r_mag', np.float64),
+         ('i_mag', np.float64),
+         ('z_mag', np.float64),
+         ('el_r_hlr', np.int),
+         ('circ_r_mag', np.float64),
+         ('pa', np.float64),
+         ('pa_align', np.float64),
+         ('ba', np.float64),
+         ('_flags', np.int),    # Original name: 'flags'
+         ('CMr', np.float64),
+         ('Cu-r', np.float64)
+])
+
