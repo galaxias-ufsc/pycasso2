bf3ebf42811e57e72112668f38bf3b2bbe1124b4
André Luiz de Amorim <streetomon@gmail.com>
2017-09-21 10:02:46 -0300

    Fixed manga stamp download for python3.

--- pycasso2/datasets.py
+++ pycasso2/datasets.py
@@ -14,7 +14,7 @@ def download_manga_cube(plate, plateifu, data_dir, wait=True, wait_time=120, ove
     file_name = 'manga-' + plateifu + '-LINCUBE.fits.gz'
 
     if overwrite == False and file_name in listdir(data_dir):
-        print 'File already downloaded'
+        print('File already downloaded')
 
     else:
 
@@ -50,7 +50,7 @@ def download_manga_cube(plate, plateifu, data_dir, wait=True, wait_time=120, ove
             time.sleep(wait_time)
 
 
-def download_manga_image(plate, ifudsgn, objid, imdir):
+def download_manga_image(plate, ifudsgn, objid, imdir, thumb=False):
     from six.moves import urllib
    
     if str(objid) + '.png' in listdir(imdir):
@@ -60,28 +60,29 @@ def download_manga_image(plate, ifudsgn, objid, imdir):
     else:
 
         url = 'https://data.sdss.org/sas/dr14/manga/spectro/redux/v2_1_2/'
-        url += str(plate).strip() + '/stack/images/' + str(ifudsgn).strip() + '.png'
+        if thumb:
+            image_name = str(ifudsgn).strip() + '_thumb.png'
+        else:
+            image_name = str(ifudsgn).strip() + '.png'
+        url += str(plate).strip() + '/stack/images/' + image_name
 
-
+        print(url)
         file_name = imdir + str(objid) + '.png'
         u = urllib.request.urlopen(url)
-        f = open(file_name, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print("Downloading: %s Bytes: %s" % (file_name, file_size))
+        file_size = int(u.getheader('Content-Length'))
+        print('Downloading: %s Bytes: %s' % (file_name, file_size))
 
         file_size_dl = 0
         block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (
-                file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8) * (len(status) + 1)
-            print(status)
-
-        f.close()
+        with open(file_name, 'wb') as f:
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+    
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status = r'%10d  [%3.2f%%]' % (
+                    file_size_dl, file_size_dl * 100. / file_size)
+                status = status + chr(8) * (len(status) + 1)
+                print(status)
