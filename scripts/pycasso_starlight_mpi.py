bef8365fa8c99689518db6df6295b4b27c3d6bdb
André Luiz de Amorim <streetomon@gmail.com>
2019-10-10 14:09:32 -0300

    Added a limited queue to avoid allocating all runs at the same time in pycasso_starlight_mpi.py.

--- scripts/pycasso_starlight_mpi.py
+++ scripts/pycasso_starlight_mpi.py
@@ -13,6 +13,7 @@ import argparse
 from mpi4py.futures import MPIPoolExecutor
 from multiprocessing import cpu_count
 from astropy import log
+from itertools import islice
 log.setLevel('DEBUG')
 
 
@@ -30,7 +31,9 @@ def parse_args():
     parser.add_argument('--config', dest='configFile', default=default_config_path,
                         help='Config file. Default: %s' % default_config_path)
     parser.add_argument('--max-workers', dest='maxWorkers', type=int, default=cpu_count() - 1,
-                        help='Number of worker processes. Defaults to the number of system processors.')
+                        help='Maximum number of worker processes. Defaults to the number of system processors - 1.')
+    parser.add_argument('--queue-length', dest='queueLength', type=int, default=-1,
+                        help='Worker queue length. Default: 10 * max workers.')
     parser.add_argument('--chunk-size', dest='chunkSize', type=int, default=2,
                         help='Grid chunk size. Default: 2.')
     parser.add_argument('--timeout', dest='timeout', type=int, default=30,
@@ -67,19 +70,34 @@ if __name__ == '__main__':
     log.info('Loading cube from %s.' % args.cubeIn[0])
     sa = SynthesisAdapter(args.cubeIn[0], cfg, new_name=args.newName)
     
+    log.debug('Creating gridfile iterator.')
     gridfiles = sa.gridIterator(chunk_size=args.chunkSize, use_errors_flags=not args.noErrorFlag,
                                 use_custom_masks=args.useCustomMasks, synth_sn=args.synthSN)
     
     exec_path = cfg.get('starlight', 'exec_path')
     timeout = args.timeout * 60
+    queue_length = args.queueLength
+    if queue_length <= 0:
+        queue_length = args.maxWorkers * 10
+        log.debug('Setting queue length to %d.' % queue_length)
     
+    log.debug('Preparing worker args.')
     map_args = ((exec_path, g, timeout, True) for g in gridfiles)
-    with MPIPoolExecutor(args.maxWorkers) as executor:
-        output_grids = executor.starmap(run_starlight, map_args, unordered=True)
 
-    for grid in output_grids:
-        log.debug('Reading results of %s.' % grid.name)
-        sa.updateSynthesis(grid)
+    log.debug('Starting execution pool.')
+    with MPIPoolExecutor(args.maxWorkers) as executor:
+        while True:
+            chunk = list(islice(map_args, queue_length))
+            if len(chunk) == 0:
+                log.debug('Exceution completed.')
+                break
+            log.debug('Dispatching %d runs.' % len(chunk))
+            output_grids = executor.starmap(run_starlight, chunk, unordered=True)
+
+            log.debug('Collecting runs.')
+            for grid in output_grids:
+                log.debug('Reading results of %s.' % grid.name)
+                sa.updateSynthesis(grid)
     
     if args.estimateError:
         print('Estimating errors from the starlight residual. Will overwrite the previous error values.')
